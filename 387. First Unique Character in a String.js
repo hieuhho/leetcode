fbabdde429591e523778d12efd12e1a97cd86a11
hieuhho <hieuhho.dev@gmail.com>
2020-06-26 10:12:01 -0700

    Solved problem 387

--- 387. First Unique Character in a String.js
+++ 387. First Unique Character in a String.js
@@ -11,8 +11,57 @@ return 2.
 
 
 Note: You may assume the string contains only lowercase English letters.
+
+O integer of firt uniq indx
+I string
+C O(n)
+E if doesn't exist return -1
+
+'bob' -> 1
+'cat' -> 0
+'aabbcc' -> -1
+
+count each letter occurance using obj
+iterate string
+if i in obj = 1
+return i
 */
 
-let firstUniqChar = (string) => {
+//Using Map
 
+let firstUniqChar = (string) => {
+  let map = new Map();
+
+  for (let i = 0; i < string.length; i += 1) {
+    if (map.has(string[i])) {
+      map.set(string[i], 2);
+    } else {
+      map.set(string[i], 1)
+    }
+  }
+  for (let j = 0; j < string.length; j += 1) {
+    if (map.get(string[j]) === 1) {
+      return j;
+    }
+  }
+  return -1;
+};
+
+//Using Object
+let firstUniqChar = (string) => {
+  let occurance = {};
+  let result = -1;
+  for (let i = 0; i < string.length; i += 1) {
+    if (!occurance[string[i]]) {
+      occurance[string[i]] = 1;
+    } else {
+      occurance[string[i]] += 1;
+    }
+  }
+  for (let j = 0; j < string.length; j += 1) {
+    if (occurance[string[j]] === 1) {
+      return j;
+    }
+  }
+  return -1;
 };
