23b0d0ba4c89ff00b2d8c733c529dc2a48d40f9f
hieuhho <hieuhho.dev@gmail.com>
2020-07-06 17:41:26 -0700

    Added Zombir in matrix

---
+++ Amazon OA | Zombie in Matrix.js
@@ -0,0 +1,48 @@
+/*
+Given a 2D grid, each cell is either a zombie 1 or a human 0. Zombies can turn adjacent (up/down/left/right) human beings into zombies every hour. Find out how many hours does it take to infect all humans?
+
+Example:
+
+Input:
+[[0, 1, 1, 0, 1],
+ [0, 1, 0, 1, 0],
+ [0, 0, 0, 0, 1],
+ [0, 1, 0, 0, 0]]
+
+Output: 2
+
+Explanation:
+At the end of the 1st hour, the status of the grid:
+[[1, 1, 1, 1, 1],
+ [1, 1, 1, 1, 1],
+ [0, 1, 0, 1, 1],
+ [1, 1, 1, 0, 1]]
+
+At the end of the 2nd hour, the status of the grid:
+[[1, 1, 1, 1, 1],
+ [1, 1, 1, 1, 1],
+ [1, 1, 1, 1, 1],
+ [1, 1, 1, 1, 1]]
+
+O integer
+I matrix
+C none
+E see above
+  input validation, empty
+*/
+
+const minHours = (matrix) => {
+  let queue = [];
+  let hours =0;
+
+  //add zombies to queue
+  matrix.forEach((row, row) => {
+    row.forEach((subject, col) => {
+      if (subject === 1) queue.push([row, col])
+    })
+  })
+
+  while (queue.length) {
+
+  }
+};
