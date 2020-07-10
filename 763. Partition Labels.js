55633e5a1d581ca3f5861976259f1c5771734150
hieuhho <hieuhho.dev@gmail.com>
2020-07-10 11:14:45 -0700

    Solved problem 763

--- 763. Partition Labels.js
+++ 763. Partition Labels.js
@@ -14,8 +14,43 @@ Note:
 
 S will have length in range [1, 500].
 S will consist of lowercase English letters ('a' to 'z') only.
+
+O array of int
+I string
+C see above
+E input validation, not letter, length of 1
+
+'abcd' -> [1,1,1,1]
+'abcda' -> [1]
+
+count occurances
+find last occurance of let
+iterate
+if last occurance = current
+end of partition
+subtract placing
+add to resutl
+placing = new partition
 */
 
 const partitionLabels = (string) => {
+  const lastIndex = {};
+  const partitions = [];
+  let place = 0;
+  let endOfPartition = 0;
 
+  for (let i = 0; i < string.length; i += 1) {
+    lastIndex[string[i]] = i;
+  }
+  for (let j = 0; j < string.length; j += 1) {
+    endOfPartition = Math.max(endOfPartition, lastIndex[string[j]]);
+    if (j === endOfPartition) {
+      partitions.push(j - place + 1);
+      place = j + 1;
+    }
+  }
+  return partitions;
 };
+
+const test1 = 'ababcbacadefegdehijhklij';
+console.log(partitionLabels(test1));
