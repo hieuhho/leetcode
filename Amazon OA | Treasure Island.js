1298d9f3e0a60c684e16b112d74229c319d9811c
hieuhho <hieuhho.dev@gmail.com>
2020-07-16 18:47:25 -0700

    Solved Treasure Island

--- Amazon OA | Treasure Island.js
+++ Amazon OA | Treasure Island.js
@@ -15,6 +15,38 @@ Output: 5
 Explanation: Route is (0, 0), (0, 1), (1, 1), (2, 1), (2, 0), (3, 0) The minimum route takes 5 steps.
 */
 
-const shortestRoute = (map) => {
+const shortestPath = (map) => {
+  let steps = 0;
+  const queue = [];
+  const directions = [[1, 0], [0, 1], [-1, 0], [0, -1]];
+  const rows = map.length;
+  const cols = map[0].length;
 
+  queue.push([0, 0]);
+  while (queue.length) {
+    steps += 1;
+    const size = queue.length;
+    for (let i = 0; i < size; i += 1) {
+      const [r, c] = queue.shift();
+      for (const [a, b] of directions) {
+        const nextRow = r + a;
+        const nextCol = c + b;
+        if (nextRow >= 0 && nextRow < rows && nextCol >= 0 && nextCol < cols) {
+          if (map[nextRow][nextCol] === 'X') return steps;
+          if (map[nextRow][nextCol] === 'O') {
+            map[nextRow][nextCol] === '*';
+            queue.push([nextRow, nextCol]);
+          }
+        }
+      }
+    }
+  }
+  return -1;
 };
+
+const test = [['O', 'O', 'O', 'O'],
+  ['D', 'O', 'D', 'O'],
+  ['O', 'O', 'O', 'O'],
+  ['X', 'D', 'D', 'O']];
+
+console.log(shortestPath(test));
