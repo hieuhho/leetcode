e3d75aef24b3fbc3f48f9894eae983dd4531619f
hieuhho <hieuhho.dev@gmail.com>
2020-07-20 18:58:48 -0700

    Solved problem 1

--- 1. Two Sum.js
+++ 1. Two Sum.js
@@ -10,3 +10,18 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
+
+const twoSum = (nums, target) => {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i += 1) {
+    const different = target - nums[i];
+    if (map[different] > -1) return [map[different], i];
+    map[nums[i]] = i;
+  }
+  return [];
+};
+
+const nums = [2, 7, 11, 15];
+const target = 9;
+console.log(twoSum(nums, target));
