cd28741b07342c80333af741b23898cc911a9f5d
hieuhho <hieuhho.dev@gmail.com>
2020-07-24 10:46:33 -0700

    Solved problem 240

--- 240. Search a 2D Matrix II.js
+++ 240. Search a 2D Matrix II.js
@@ -20,5 +20,27 @@ Given target = 20, return false.
 */
 
 const searchMatrix = (matrix, target) => {
+  if (matrix.length === 0 || matrix[0].length === 0) return false;
 
+  let i = 0;
+  let j = matrix[0].length - 1;
+
+  while (i < matrix.length && j >= 0) {
+    if (matrix[i][j] === target) return true;
+    if (matrix[i][j] > target) j -= 1;
+    else i += 1;
+  }
+  return false;
 };
+
+const matrix = [
+  [1, 4, 7, 11, 15],
+  [2, 5, 8, 12, 19],
+  [3, 6, 9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30],
+];
+
+const target = 20;
+
+console.log(searchMatrix(matrix, target));
