ace793f38c49dd9229e8751ab03332a4df830380
hieuhho <hieuhho.dev@gmail.com>
2020-07-29 18:06:30 -0700

    Solved problem 59

--- 59. Spiral Matrix II.js
+++ 59. Spiral Matrix II.js
@@ -10,8 +10,33 @@ Output:
  [ 8, 9, 4 ],
  [ 7, 6, 5 ]
 ]
+
+O matrix
+I positive int
+C none
+E input val -> return []
+
 */
 
 const generateMatrix = (n) => {
+  const dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+  const matrix = [...Array(n)].map(() => Array(n).fill(null));
+  const traverse = [n, n - 1];
+  let count = 1;
+  let dir = 0;
+  let i = 0;
+  let j = -1;
 
+  while (traverse[dir % 2] > 0) {
+    for (let k = 0; k < traverse[dir % 2]; k += 1) {
+      i += dirs[dir][0];
+      j += dirs[dir][1];
+      matrix[i][j] = count++;
+    }
+    traverse[dir % 2] -= 1;
+    dir = (dir + 1) % 4;
+  }
+  return matrix;
 };
+
+console.log(generateMatrix(3));
