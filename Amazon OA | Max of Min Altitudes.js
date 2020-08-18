5c29c4a3483d3f93efd548fd1e349a39f33af1ab
hieuhho <hieuhho.dev@gmail.com>
2020-08-17 17:25:35 -0700

    Solved Max of Min Altitudes

--- Amazon OA | Max of Min Altitudes.js
+++ Amazon OA | Max of Min Altitudes.js
@@ -33,5 +33,27 @@ Return the max of that, so 4.
 */
 
 const maxMinPath = (arr) => {
+  const dfs = new Array(arr.length).fill(0).map((i) => new Array(arr[0].length).fill(0));
+  dfs[0][0] = arr[0][0];
+  const r = arr.length;
+  const c = arr[0].length;
 
+  for (let i = 0; i < r; i += 1) {
+    for (let j = 0; j < c; j += 1) {
+      if (i === 0 && j - 1 >= 0) {
+        dfs[i][j] = Math.min(arr[i][j], dfs[i][j - 1]);
+      } else if (j === 0 && i - 1 >= 0) {
+        dfs[i][j] = Math.min(arr[i][j], dfs[i - 1][j]);
+      } else if (i - 1 >= 0 && j - 1 >= 0) {
+        const top = Math.min(dfs[i - 1][j], arr[i][j]);
+        const left = Math.min(dfs[i][j - 1], arr[i][j]);
+        dfs[i][j] = Math.max(top, left);
+      }
+    }
+  }
+  return dfs[dfs.length - 1][dfs[0].length - 1];
 };
+
+const test = [[5, 1],
+  [4, 5]];
+console.log(maxMinPath(test));
