20c76e0272e27be60c625e629e92dce85270ad87
hieuhho <hieuhho.dev@gmail.com>
2020-08-27 21:48:44 -0700

    Solved problem 102

--- 102. Binary Tree Level Order Traversal.js
+++ 102. Binary Tree Level Order Traversal.js
@@ -17,6 +17,34 @@ return its level order traversal as:
 Accepted
 */
 
-const levelOrder = (root) => {
+function TreeNode(val, left, right) {
+  this.val = (val === undefined ? 0 : val);
+  this.left = (left === undefined ? null : left);
+  this.right = (right === undefined ? null : right);
+}
 
+const levelOrder = (root) => {
+  if (!root) return [];
+
+  const stack = [];
+  const result = [];
+
+  stack.push(root);
+
+  while (stack.length > 0) {
+    const size = stack.length;
+    const temp = [];
+
+    for (let i = 0; i < size; i += 1) {
+      const node = stack.shift();
+      temp.push(node.val);
+      if (node.left) stack.push(node.left);
+      if (node.right) stack.push(node.right);
+    }
+    result.push(temp);
+  }
+  return result;
 };
+
+const test = [3, 9, 20, null, null, 15, 7];
+console.log(levelOrder(test));
