12df74ee224ffd362e14e0fc932484b729539e06
hieuhho <hieuhho.dev@gmail.com>
2020-08-31 15:01:02 -0700

    Solved problem 1254 (DFS)

--- 1254. Number of Closed Islands.js
+++ 1254. Number of Closed Islands.js
@@ -35,6 +35,49 @@ Constraints:
 0 <= grid[i][j] <=1
 */
 
-const closedIslands = (grid) => {
+const dfs = (row, col, rows, cols, grid) => {
+  if (row < 0 || col < 0 || row >= rows || col >= cols) return false;
+  if (grid[row][col] === 1) return true;
 
+  grid[row][col] = 1;
+
+  const top = dfs(row - 1, col, rows, cols, grid);
+  const bot = dfs(row + 1, col, rows, cols, grid);
+  const left = dfs(row, col - 1, rows, cols, grid);
+  const right = dfs(row, col + 1, rows, cols, grid);
+
+  if (top && bot && left && right) return true;
+  grid[row][col] = 0;
+  return false;
 };
+
+const closedIslands = (grid) => {
+  const rows = grid.length;
+  const cols = grid[0].length;
+
+  let count = 0;
+
+  for (let i = 0; i < rows - 1; i += 1) {
+    for (let j = 0; j < cols - 1; j += 1) {
+      if (grid[i][j] === 0) {
+        if (dfs(i, j, rows, cols, grid)) count += 1;
+      }
+    }
+  }
+  return count;
+};
+
+const test1 = [[1, 1, 1, 1, 1, 1, 1, 0], [1, 0, 0, 0, 0, 1, 1, 0], [1, 0, 1, 0, 1, 1, 1, 0], [1, 0, 0, 0, 0, 1, 0, 1], [1, 1, 1, 1, 1, 1, 1, 0]];
+console.log(closedIslands(test1));
+
+const test2 = [[0, 0, 1, 0, 0], [0, 1, 0, 1, 0], [0, 1, 1, 1, 0]];
+console.log(closedIslands(test2));
+
+const test3 = [[1, 1, 1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 0, 0, 1],
+  [1, 0, 1, 1, 1, 0, 1],
+  [1, 0, 1, 0, 1, 0, 1],
+  [1, 0, 1, 1, 1, 0, 1],
+  [1, 0, 0, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1]];
+console.log(closedIslands(test3));
