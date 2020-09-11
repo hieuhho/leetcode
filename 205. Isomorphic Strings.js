061c18276f447f59b28481a86abf3777f11b4de8
hieuhho <hieuhho.dev@gmail.com>
2020-09-11 14:43:03 -0700

    Solved problem 205

--- 205. Isomorphic Strings.js
+++ 205. Isomorphic Strings.js
@@ -24,5 +24,28 @@ You may assume both s and t have the same length.
 */
 
 const isIsomorphic = (s, t) => {
-
+  if (s.length !== t.length) return false;
+  if (new Set(s).size !== new Set(t).size) return false;
+  const map = new Map();
+  for (let i = 0; i < t.length; i += 1) {
+    if (map.has(s[i]) && map.get(s[i]) !== t[i]) return false;
+    map.set(s[i], t[i]);
+  }
+  return true;
 };
+
+const s = 'egg';
+const t = 'add';
+console.log(isIsomorphic(s, t));
+
+const s2 = 'foo';
+const t2 = 'bar';
+console.log(isIsomorphic(s2, t2));
+
+const s3 = 'paper';
+const t3 = 'title';
+console.log(isIsomorphic(s3, t3));
+
+const s4 = 'aba';
+const t4 = 'baa';
+console.log(isIsomorphic(s4, t4));
