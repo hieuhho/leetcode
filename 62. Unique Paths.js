82a6846f1a8e9fa40ebf5b959ce87be00ca301d2
hieuhho <hieuhho.dev@gmail.com>
2020-09-15 20:36:15 -0700

    Solved problem 62

--- 62. Unique Paths.js
+++ 62. Unique Paths.js
@@ -16,6 +16,7 @@ From the top-left corner, there are a total of 3 ways to reach the bottom-right 
 1. Right -> Right -> Down
 2. Right -> Down -> Right
 3. Down -> Right -> Right
+
 Example 2:
 
 Input: m = 7, n = 3
@@ -27,6 +28,31 @@ Constraints:
 It's guaranteed that the answer will be less than or equal to 2 * 10 ^ 9.
 */
 
-const uniquePath = (m, n) => {
-
+// dfs
+const uniquePathsDFS = (m, n) => {
+  const dfs = (i = m - 1, j = n - 1) => {
+    if (!i || !j) return 1;
+    return dfs(i - 1, j) + dfs(i, j - 1);
+  };
+  return dfs();
 };
+
+// bottom up
+const uniquePaths = (m, n) => {
+  const arr = Array(n).fill(1);
+  for (let i = 1; i < m; i += 1) {
+    const previous = [...arr];
+    for (let j = 1; j < n; j += 1) {
+      arr[j] = previous[j] + arr[j - 1];
+    }
+  }
+  return arr[n - 1];
+};
+
+const m = 3;
+const n = 2;
+console.log(uniquePaths(m, n));
+
+const m2 = 7;
+const n2 = 3;
+console.log(uniquePaths(m2, n2));
