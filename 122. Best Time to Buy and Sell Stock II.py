817ab0186a0ec37a6a3dc94ccefa6a93a342fea3
hieuhho <hieuhho.dev@gmail.com>
2020-09-21 13:02:53 -0700

    Solved problem 122 python

--- 122. Best Time to Buy and Sell Stock II.py
+++ 122. Best Time to Buy and Sell Stock II.py
@@ -28,5 +28,13 @@
 # 1 <= prices.length <= 3 * 10 ^ 4
 # 0 <= prices[i] <= 10 ^ 4
 
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+def maxProfit(prices):
+  if not prices or len(prices) is 1: return 0
+  profit = 0
+  for i in range(1, len(prices)):
+    if (prices[i] > prices[i - 1]):
+      profit += prices[i] - prices[i - 1]
+  return profit
+
+test1 = [7,1,5,3,6,4]
+print(maxProfit(test1))
