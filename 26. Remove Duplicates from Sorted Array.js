7dc1142fb1d2de87b2e8db4ebe3ae31293d6756e
hieuhho <hieuhho.dev@gmail.com>
2020-09-21 12:30:40 -0700

    Solved problem 26

--- 26. Remove Duplicates from Sorted Array.js
+++ 26. Remove Duplicates from Sorted Array.js
@@ -37,5 +37,18 @@ for (int i = 0; i < len; i++) {
 */
 
 const removeDuplicates = (nums) => {
-
+  if (nums.length === 0) return 0;
+  let i = 0;
+  for (let j = 1; j < nums.length; j += 1) {
+    if (nums[j] !== nums[i]) {
+      i += 1;
+      nums[i] = nums[j];
+    }
+  }
+  return i + 1;
 };
+
+const test = [1, 1, 2];
+const test2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+console.log(removeDuplicates(test));
+console.log(removeDuplicates(test2));
