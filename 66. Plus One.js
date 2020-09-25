65c347dab80ca3a3ebd976bc6c5d4ef13f4ad94f
hieuhho <hieuhho.dev@gmail.com>
2020-09-25 11:42:08 -0700

    Solved problem 66

--- 66. Plus One.js
+++ 66. Plus One.js
@@ -27,5 +27,18 @@ Constraints:
 */
 
 const plusOne = (digits) => {
-
+  for (let i = digits.length - 1; i >= 0; i -= 1) {
+    if (digits[i] < 9) {
+      digits[i] = digits[i] + 1;
+      return digits;
+    }
+    digits[i] = 0;
+  }
+  digits.unshift(1);
+  return digits;
 };
+
+const test1 = [1, 2, 3];
+const test2 = [4, 3, 2, 1];
+console.log(plusOne(test1));
+console.log(plusOne(test2));
