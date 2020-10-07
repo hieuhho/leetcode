c7f10e4879460f72a135035b97c5e20040abc5de
hieuhho <hieuhho.dev@gmail.com>
2020-10-07 09:01:39 -0700

    Solved problem 15

--- 15. 3Sum.js
+++ 15. 3Sum.js
@@ -22,5 +22,35 @@ Constraints:
 -105 <= nums[i] <= 105
 */
 const threeSum = (nums) => {
-
+  const result = [];
+  if (nums.length < 3) return result;
+  nums = nums.sort((a, b) => a - b);
+  for (let start = 0; start < nums.length - 2; start += 1) {
+    if (nums[start] > 0) break;
+    if (start > 0 && nums[start] === nums[start - 1]) continue;
+    let slide = start + 1;
+    let end = nums.length - 1;
+    while (slide < end) {
+      const sum = nums[start] + nums[slide] + nums[end];
+      if (sum === 0) {
+        result.push([nums[start], nums[slide], nums[end]]);
+        while (nums[slide] === nums[slide + 1]) slide += 1;
+        while (nums[end] === nums[end - 1]) end -= 1;
+        slide += 1;
+        end -= 1;
+      } else if (sum < 0) {
+        slide += 1;
+      } else {
+        end -= 1;
+      }
+    }
+  }
+  return result;
 };
+
+const test1 = [-1, 0, 1, 2, -1, -4];
+const test2 = [0];
+const test3 = [];
+console.log(threeSum(test1));
+console.log(threeSum(test2));
+console.log(threeSum(test3));
