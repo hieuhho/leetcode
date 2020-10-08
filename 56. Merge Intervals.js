e4d5e6230fe639afa10a6f1b56aeee6debbdeb20
hieuhho <hieuhho.dev@gmail.com>
2020-10-08 10:18:57 -0700

    Solved problem 56

--- 56. Merge Intervals.js
+++ 56. Merge Intervals.js
@@ -19,5 +19,25 @@ intervals[i][0] <= intervals[i][1]
 */
 
 const merge = (intervals) => {
+  if (!intervals.length) return intervals;
+  intervals = intervals.sort((a, b) => a[0] - b[0]);
+  let lowest = intervals[0];
+  const result = [lowest];
 
+  for (const current of intervals) {
+    if (current[0] <= lowest[1]) {
+      lowest[1] = Math.max(current[1], lowest[1]);
+    } else {
+      result.push(current);
+      lowest = current;
+    }
+  }
+  return result;
 };
+
+const test1 = [[1, 3], [2, 6], [8, 10], [15, 18]];
+const test2 = [[1, 4], [4, 5]];
+const test3 = [[1, 2], [4, 6], [3, 5]];
+console.log(merge(test1));
+console.log(merge(test2));
+console.log(merge(test3));
