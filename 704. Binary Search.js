0e2303fcaddac7976aebca75b1ffb292ce5f71d8
hieuhho <hieuhho.dev@gmail.com>
2020-10-19 10:49:50 -0700

    Solved problem 704

--- 704. Binary Search.js
+++ 704. Binary Search.js
@@ -20,5 +20,25 @@ n will be in the range [1, 10000].
 The value of each element in nums will be in the range [-9999, 9999].
 */
 const search = (nums, target) => {
-
+  let start = 0;
+  let end = nums.length - 1;
+  while (start <= end) {
+    const mid = (start + end) / 2 | 0;
+    if (nums[mid] === target) return mid;
+    if (nums[mid] > target) end = mid - 1;
+    else { start = mid + 1; }
+  }
+  return -1;
 };
+
+const nums1 = [-1, 0, 3, 5, 9, 12];
+const target1 = 9;
+console.log(search(nums1, target1));
+
+const nums2 = [-1, 0, 3, 5, 9, 12];
+const target2 = 2;
+console.log(search(nums2, target2));
+
+const nums3 = [5];
+const target3 = 5;
+console.log(search(nums3, target3));
