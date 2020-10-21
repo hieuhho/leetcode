7d7c3a341296067576bc10165603b6c47368d2d5
hieuhho <hieuhho.dev@gmail.com>
2020-10-21 09:47:53 -0700

    Solved problem 112.py

--- 112. Path Sum.py
+++ 112. Path Sum.py
@@ -15,4 +15,14 @@
 # 7    2      1
 # return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
 
-def hasPathSum(self, root, sum):
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def hasPathSum(root, sum):
+  if not root: return false
+  if not root.left and not root.right and root.val == sum: return true
+  remain = sum - root.val
+  return self.hasPathSum(root.left, remain) or self.hasPathSum(root.right, remain)
