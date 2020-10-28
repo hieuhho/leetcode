1c124c2676bb4a59888ff0f116b17ce2a0d9dbca
hieuhho <hieuhho.dev@gmail.com>
2020-10-28 10:51:31 -0700

    Solved problem 234

--- 234. Palindrome Linked List.js
+++ 234. Palindrome Linked List.js
@@ -13,5 +13,27 @@ Follow up:
 Could you do it in O(n) time and O(1) space?
 */
 const isPalindrome = (head) => {
-
+  if (head === null) return true;
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  if (fast) slow = slow.next;
+  let prev = null;
+  while (slow) {
+    const { next } = slow;
+    slow.next = prev;
+    prev = slow;
+    slow = next;
+  }
+  console.log('slow: ', slow);
+  fast = head;
+  while (prev) {
+    if (prev.val !== fast.val) return false;
+    prev = prev.next;
+    fast = fast.next;
+  }
+  return true;
 };
